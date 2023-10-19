f11635aa2f73c1a3852053b15fbd42fa6babdf30
Ricardo Pereira <patresio@gmail.com>
2023-10-19 14:59:13 -0300

    feat: add auth handler to header component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import {
   HomeIcon,
   ListOrderedIcon,
   LogInIcon,
+  LogOutIcon,
   MenuIcon,
   PercentCircleIcon,
   ShoppingCartIcon,
@@ -9,8 +12,21 @@ import {
 import { Button } from "./button";
 import { Card } from "./card";
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import { Separator } from "./separator";
 
 const Header = () => {
+  const { status, data } = useSession();
+
+  const handleLoginClick = async () => {
+    await signIn();
+  };
+
+  const handleLogoutClick = async () => {
+    await signOut();
+  };
+
   return (
     <div>
       <Card className="flex justify-between p-[1.875rem]">
@@ -26,11 +42,47 @@ const Header = () => {
               Menu
             </SheetHeader>
 
-            <div className="mt-2 flex-col gap-3">
-              <Button variant="outline" className="w-full justify-start gap-2">
-                <LogInIcon size={16} />
-                Fazer login
-              </Button>
+            {status === "authenticated" && data?.user && (
+              <div className="flex flex-col">
+                <div className="flex items-center gap-2 py-4">
+                  <Avatar>
+                    <AvatarFallback>
+                      {data.user.name?.[0].toUpperCase()}
+                    </AvatarFallback>
+
+                    {data.user.image && <AvatarImage src={data.user.image} />}
+                  </Avatar>
+                  <div className="flex flex-col">
+                  <p className="font-medium">{data.user.name}</p>
+                  <p className="text-sm opacity-75">Boas compras!</p>
+                  </div>
+                </div>
+                <Separator />
+              </div>
+            )}
+
+            <div className="mt-4 flex-col gap-3">
+              {status === "unauthenticated" && (
+                <Button
+                  onClick={handleLoginClick}
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <LogInIcon size={16} />
+                  Fazer login
+                </Button>
+              )}
+
+              {status === "authenticated" && (
+                <Button
+                  onClick={handleLogoutClick}
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <LogOutIcon size={16} />
+                  Fazer logout
+                </Button>
+              )}
 
               <Button variant="outline" className="w-full justify-start gap-2">
                 <HomeIcon size={16} />
