4a6d65408ccf26ee6fa1655eb538ebf0da6210b9
Ricardo Pereira <patresio@gmail.com>
2023-10-20 07:59:12 -0300

    feat: add second banner home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -26,7 +26,17 @@ export default async function Home() {
     <Categories />
     </div>
     <div className="mt-8">
+      <p className="font-bold pl-5 uppercase mb-3">Ofertas</p>
       <ProductList products={deals}/>
     </div>
+
+    <Image 
+    src="/banner-home-02.png"
+    width={0}
+    height={0}
+    className="h-auto w-full px-5"
+    sizes="100vw"
+    alt="Até 55% de desconto em mouses!"
+    />
   </div>;
 }
