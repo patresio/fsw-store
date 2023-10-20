1a698c9806aba153ddc8ea817ee4e6e9107474f2
Ricardo Pereira <patresio@gmail.com>
2023-10-20 07:16:27 -0300

    feat: add computeProductTotalPrice

--- src/helpers/product.ts
+++ src/helpers/product.ts
@@ -1,5 +1,21 @@
 import { Product } from "@prisma/client";
 
-const computeProductTotalPrice = (product: Product) => {
-
+interface ProductWithTotalPrice extends Product {
+    totalPrice: number;
 }
+
+export const computeProductTotalPrice = (product: Product): ProductWithTotalPrice => {
+    if (product.discountPercentage === 0) {
+        return {
+            ...product,
+            totalPrice: Number(product.basePrice),
+        };
+    }
+
+    const totalPrice = Number(product.basePrice) * (product.discountPercentage / 100);
+
+    return {
+        ...product,
+        totalPrice,
+    };
+};
