ca169599d81a76d623dcf2e97a685be254861ec8
Ricardo Pereira <patresio@gmail.com>
2023-10-20 08:12:36 -0300

    feat: add two home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Categories from './components/categories';
 import { prismaClient } from "@/lib/prisma";
 import ProductList from './components/product-list';
+import SectionTitle from "./components/section-title";
+import PromoBanner from "./components/promo-banner";
 
 export default async function Home() {
 
@@ -13,30 +15,35 @@ export default async function Home() {
     }
   })
 
+  const keyboards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "keyboards",
+      }
+    }
+  })
+
   return <div>
-    <Image 
+    <PromoBanner 
     src="/banner-home-01.png"
-    width={0}
-    height={0}
-    className="h-auto w-full px-5"
-    sizes="100vw"
     alt="Até 55% de desconto esse mês"
     />
     <div className="mt-8 px-5">
     <Categories />
     </div>
     <div className="mt-8">
-      <p className="font-bold pl-5 uppercase mb-3">Ofertas</p>
+      <SectionTitle>Ofertas</SectionTitle>
       <ProductList products={deals}/>
     </div>
 
-    <Image 
+    <PromoBanner 
     src="/banner-home-02.png"
-    width={0}
-    height={0}
-    className="h-auto w-full px-5"
-    sizes="100vw"
     alt="Até 55% de desconto em mouses!"
     />
+
+    <div className="mt-8">
+      <SectionTitle>Teclados</SectionTitle>
+      <ProductList products={keyboards}/>
+    </div>
   </div>;
 }
