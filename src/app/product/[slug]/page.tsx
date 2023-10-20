f0ea24da7441719b377e3f9c0625a95c8e003610
Ricardo Luis Barbosa Pereira <patresio@gmail.com>
2023-10-20 20:18:10 -0300

    feat: add figma components

---
+++ src/app/product/[slug]/page.tsx
@@ -0,0 +1,28 @@
+import { prismaClient } from "@/lib/prisma";
+import ProductImages from './components/product-images';
+
+interface ProductDetailsPageProps {
+    params: {
+        slug: string
+    }
+}
+const ProductDetailsPage = async ({params: {slug}}: ProductDetailsPageProps ) => {
+    
+    const product = await prismaClient.product.findFirst({
+        where: {
+            slug: slug
+        }
+    })
+    
+    if (!product) {
+      return null;
+    }
+
+    return ( 
+        <div>
+            <ProductImages imageUrls={product.imageUrls} name={product.name}/>
+        </div>
+     );
+}
+ 
+export default ProductDetailsPage;
--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -2,6 +2,7 @@ import { ProductWithTotalPrice } from "@/helpers/product";
 import Image from "next/image";
 import { Badge } from "./badge";
 import { ArrowDownIcon } from "lucide-react";
+import Link from "next/link";
 
 interface ProductItemProps {
     product: ProductWithTotalPrice
@@ -9,6 +10,7 @@ interface ProductItemProps {
 
 const ProductItem = ({product}: ProductItemProps) => {
     return ( 
+        <Link href={`/product/${product.slug}`}>
         <div className="flex flex-col gap-4">
             <div className="relative flex bg-accent rounded-lg h-[170px] w-full items-center justify-center">
                 <Image 
@@ -47,6 +49,7 @@ const ProductItem = ({product}: ProductItemProps) => {
                 )}
             </div>
         </div>
+        </Link>
      );
 }
  
