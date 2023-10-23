8d3196cf799048591e0ca337d91791c3ddf0e4d0
Ricardo Luis Barbosa Pereira <patresio@gmail.com>
2023-10-23 18:48:46 -0300

    teste

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -11,12 +11,18 @@ import {
 } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
-import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "./sheet";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 import { Separator } from "./separator";
 import Link from "next/link";
-import Cart from './cart';
+import Cart from "./cart";
 
 const Header = () => {
   const { status, data } = useSession();
@@ -55,8 +61,8 @@ const Header = () => {
                     {data.user.image && <AvatarImage src={data.user.image} />}
                   </Avatar>
                   <div className="flex flex-col">
-                  <p className="font-medium">{data.user.name}</p>
-                  <p className="text-sm opacity-75">Boas compras!</p>
+                    <p className="font-medium">{data.user.name}</p>
+                    <p className="text-sm opacity-75">Boas compras!</p>
                   </div>
                 </div>
                 <Separator />
@@ -85,46 +91,56 @@ const Header = () => {
                   Fazer logout
                 </Button>
               )}
-              
+
               <SheetClose asChild>
-              <Link href='/' className="w-full">
-              <Button variant="outline" className="w-full justify-start gap-2">
-                <HomeIcon size={16} />
-                Início
-              </Button>
-              </Link>
+                <Link href="/" className="w-full">
+                  <Button
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
+                    <HomeIcon size={16} />
+                    Início
+                  </Button>
+                </Link>
               </SheetClose>
-              
 
-              <Button variant="outline" className="w-full justify-start gap-2">
-                <PercentCircleIcon size={16} />
-                Ofertas
-              </Button>
-
-              
               <SheetClose asChild>
-              <Link href='/catalog' className="w-full">
-                <Button variant="outline"className="w-full justify-start gap-2">
-                  <ListOrderedIcon size={16} />
-                  Catálogo
-                </Button>
-              </Link>
+                <Link href="/deals" className="w-full">
+                  <Button
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
+                    <PercentCircleIcon size={16} />
+                    Ofertas
+                  </Button>
+                </Link>
+              </SheetClose>
+
+              <SheetClose asChild>
+                <Link href="/catalog" className="w-full">
+                  <Button
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
+                    <ListOrderedIcon size={16} />
+                    Catálogo
+                  </Button>
+                </Link>
               </SheetClose>
             </div>
           </SheetContent>
         </Sheet>
 
-        <Link href='/'>        
-        <h1 className="text-lg font-semibold">
-          <span className="text-primary">FSW</span> Store
-        </h1>
+        <Link href="/">
+          <h1 className="text-lg font-semibold">
+            <span className="text-primary">FSW</span> Store
+          </h1>
         </Link>
-
 
         <Sheet>
           <SheetTrigger asChild>
             <Button size="icon" variant="outline">
-            <ShoppingCartIcon />
+              <ShoppingCartIcon />
             </Button>
           </SheetTrigger>
 
@@ -132,7 +148,6 @@ const Header = () => {
             <Cart />
           </SheetContent>
         </Sheet>
-        
       </Card>
     </div>
   );
